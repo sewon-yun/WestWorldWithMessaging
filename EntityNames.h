768fc79914afac7daca38ad470bd57b7287fad32
sewon <sewon0702@naver.com>
2022-10-05 23:42:14 +0900

    초기 커밋.

---
+++ EntityNames.h
@@ -0,0 +1,35 @@
+#ifndef NAMES_H
+#define NAMES_H
+
+#include <string>
+
+enum 
+{
+  ent_Miner_Bob,
+
+  ent_Elsa,
+
+  ent_Evan
+};
+
+inline std::string GetNameOfEntity(int n)
+{
+  switch(n)
+  {
+  case ent_Miner_Bob:
+
+    return "Miner Bob";
+
+  case ent_Elsa:
+    
+    return "Elsa"; 
+
+  case ent_Evan:
+    return "Evan";
+  default:
+
+    return "UNKNOWN!";
+  }
+}
+
+#endif
